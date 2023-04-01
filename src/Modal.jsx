e0e8c903c4e69500fe52391f5d5d5e2a0a9e628f
Khaled-Ballour <ballourk@gmail.com>
2023-04-01 16:39:23 +0200

    the Modal functionality

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,17 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Modal = () => {
-  return <div>Module</div>;
+  const { isModalOpen, closeModal } = useGlobalContext();
+  return (
+    <div className={isModalOpen ? 'modal-overlay show-modal' : 'show-modal'}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Modal;
